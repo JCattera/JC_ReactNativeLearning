85712bcf08fb07040e47490d75ba2f67e920c5a2
JCattera <jfcatterall@gmail.com>
2020-11-13 16:10:32 -0500

    Initial commit

---
+++ basics-01-starting-project/App.js
@@ -0,0 +1,46 @@
+import React, { useState} from 'react';
+import { StyleSheet, View, FlatList, Button} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, updateCourseGoals] = useState([]);
+  const [modalVisibility, changeModalVisibility] = useState(false);
+
+  const addGoalHandler = (newGoalTxt) => {
+    //updateCourseGoals([...courseGoals,enteredGoal]) // not ideal if we want 100% up-to-date
+    updateCourseGoals(currentGoals => [
+      ...currentGoals, 
+      { uid: Math.random().toString(), value: newGoalTxt}
+    ]);
+    changeModalVisibility(false);
+  };
+  const removeGoalHandler = goalId => {
+    updateCourseGoals(currentGoals=> {
+      return currentGoals.filter((goal) => goal.uid !== goalId)
+    })
+  };
+  return (
+    <View style={styles.screen}>
+      <Button title="Add Goal" onPress={() => changeModalVisibility(true)}/>
+      <GoalInput onAddGoal={addGoalHandler} isVisible={modalVisibility}/>
+      <FlatList 
+        keyExtractor={(item, index) => item.uid}
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem 
+            id={itemData.item.uid} 
+            onDelete={removeGoalHandler} 
+            title={itemData.item.value}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }
+});

126f83b1fd0b03aaf8abf0d2689d2ec05d78e875
JC054021 <jcatterall@mayerbrown.com>
2021-03-26 15:08:48 -0400

    changed app to use flatlist

--- Section4_GuessANumberApp/screens/GameScreen.js
+++ Section4_GuessANumberApp/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
@@ -20,10 +20,10 @@ const generateRandomBetween = (min, max, exclude) => {
     }
 };
 
-const renderListItem = (value, roundNumber) => (
-    <View key={value} style={styles.listItem}>
-        <BodyText>#{roundNumber}</BodyText>
-        <BodyText>{value}</BodyText>
+const renderListItem = (listLength, itemData) => (
+    <View style={styles.listItem}>
+        <BodyText>#{listLength-itemData.index}</BodyText>
+        <BodyText>{itemData.item}</BodyText>
     </View>
 );
 
@@ -31,7 +31,7 @@ const renderListItem = (value, roundNumber) => (
 const GameScreen = props => {
     const initialGuess = generateRandomBetween(1, 100, props.userChoice)
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
-    const [pastGuesses, setPastGuesses] = useState([initialGuess]);
+    const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
@@ -62,7 +62,7 @@ const GameScreen = props => {
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
         setCurrentGuess(nextNumber);
         //setRounds(currentRounds => currentRounds + 1);
-        setPastGuesses(curPastGuesses => [nextNumber, ...curPastGuesses])
+        setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses])
     };
     return (
         <View style={styles.screen}>
@@ -77,10 +77,15 @@ const GameScreen = props => {
 
                 </MainButton>
             </Card>
-            <View style={styles.list}>
-                <ScrollView>
+            <View style={styles.listContainer}>
+                {/* <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index)  => renderListItem(guess, pastGuesses.length - index))}
-                </ScrollView>
+                </ScrollView> */}
+                <FlatList 
+                    keyExtractor={(item)=>item} 
+                    data={pastGuesses} 
+                    renderItem={renderListItem.bind(this, pastGuesses.length)}
+                    contentContainerStyle={styles.list}></FlatList>
             </View>
         </View>
     )
@@ -99,9 +104,14 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         marginTop: 20
     },
-    list: {
+    listContainer: {
         flex: 1,
-        width: '80%'
+        width: '60%'
+    },
+    list:{
+        flexGrow: 1, 
+        // alignItems: 'center',
+        justifyContent: 'flex-end' 
     },
     listItem: {
         borderColor: '#ccc',
@@ -110,7 +120,8 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         backgroundColor: 'white',
         flexDirection: 'row',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        width: '100%'
     }
 
 

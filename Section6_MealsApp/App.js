ccd9e3c124aaddfdf2677880bc2f60a56b5a0d09
JC054021 <jcatterall@mayerbrown.com>
2021-04-23 14:04:39 -0400

    beginning work on meals app

--- Section6_MealsApp/App.js
+++ Section6_MealsApp/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+import MealsNavigator from './navigation/MealsNavigator';
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -21,8 +22,6 @@ export default function App() {
   };
 
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator/>
   );
 };

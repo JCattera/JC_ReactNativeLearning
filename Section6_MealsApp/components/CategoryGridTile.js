0593a8464d39178c80b0c2a25cd66c8adff37e00
JCattera <jfcatterall@gmail.com>
2021-05-14 09:09:01 -0400

    fixing shadows

--- Section6_MealsApp/components/CategoryGridTile.js
+++ Section6_MealsApp/components/CategoryGridTile.js
@@ -35,7 +35,9 @@ const styles = StyleSheet.create({
     height: 150,
     justifyContent: 'space-between',
     borderRadius: 15,
-    overflow: 'hidden',
+    overflow:
+      Platform.OS === 'android' && Platform.Version > 21 ? 'hidden' : '',
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -44,7 +46,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',

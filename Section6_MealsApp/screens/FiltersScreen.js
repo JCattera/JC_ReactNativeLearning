d93a588d0457ad08c7e0c902dd2d8c11cbd7a1b7
JCattera <jfcatterall@gmail.com>
2021-06-14 17:51:26 -0400

    finished Unit 6

--- Section6_MealsApp/screens/FiltersScreen.js
+++ Section6_MealsApp/screens/FiltersScreen.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-
+import { useDispatch } from 'react-redux';
 import HeaderButton from '../components/HeaderButton';
 import Colors from '../constants/Colors';
+import { setFilters } from '../store/actions/mealsActions';
 
 const FilterItem = (props) => {
   return (
@@ -25,7 +26,7 @@ const FiltersScreen = (props) => {
   const [isVegetarian, setIsVegetarian] = useState(false);
   const [isVegan, setIsVegan] = useState(false);
   const [isLactoseFree, setIsLactoseFree] = useState(false);
-
+  const dispatch = useDispatch();
   const saveFilters = useCallback(() => {
     const appliedFilters = {
       glutenFree: isGlutenFree,
@@ -33,8 +34,9 @@ const FiltersScreen = (props) => {
       vegetarian: isVegetarian,
       vegan: isVegan,
     };
-    console.log(appliedFilters);
-  }, [isGlutenFree, isLactoseFree, isVegetarian, isVegan]);
+
+    dispatch(setFilters(appliedFilters));
+  }, [isGlutenFree, isLactoseFree, isVegetarian, isVegan, dispatch]);
 
   useEffect(() => {
     navigation.setParams({ save: saveFilters });

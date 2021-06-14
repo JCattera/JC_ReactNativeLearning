56ef94075fb7b4aca4f1fea3a4a79da715693ca8
JCattera <jfcatterall@gmail.com>
2021-06-14 17:51:26 -0400

    Finished Unit 7

--- Section6_MealsApp/screens/FavoritesScreen.js
+++ Section6_MealsApp/screens/FavoritesScreen.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import MealList from '../components/MealList';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import { useSelector } from 'react-redux';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = (props) => {
   const faveMeals = useSelector((state) => state.meals.favoriteMeals);
-
-  return (
-    <MealList
-      displayedMeals={dummyFavorites}
-      navigateFunction={props.navigation.navigate}
-    />
-  );
+  if (faveMeals.length === 0 || !faveMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText style={styles.text}>
+          No favorite meals found. Start adding some!
+        </DefaultText>
+      </View>
+    );
+  } else {
+    return (
+      <MealList
+        displayedMeals={faveMeals}
+        navigateFunction={props.navigation.navigate}
+      />
+    );
+  }
 };
 
 FavoritesScreen.navigationOptions = (navData) => {
@@ -34,4 +45,14 @@ FavoritesScreen.navigationOptions = (navData) => {
   };
 };
 
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
 export default FavoritesScreen;
--- Section6_MealsApp/store/reducers/meals.js
+++ Section6_MealsApp/store/reducers/meals.js
@@ -1,4 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/mealsActions';
 
 const initialState = {
   meals: MEALS,
@@ -7,7 +8,51 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TOGGLE_FAVORITE: {
+      const mealIndex = state.favoriteMeals.findIndex(
+        (meal) => meal.id === action.mealId
+      );
+      if (mealIndex >= 0) {
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(mealIndex, 1);
+        return { ...state, favoriteMeals: updatedFavMeals };
+      } else {
+        const meal = state.meals.find((meal) => meal.id === action.mealId);
+        return {
+          ...state,
+          favoriteMeals: state.favoriteMeals.concat(meal),
+        };
+      }
+    }
+    case SET_FILTERS: {
+      const appliedFilters = action.filters;
+      const newFilteredMeals = state.meals.filter((meal) => {
+        // if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+        //   return false;
+        // }
+        // if (appliedFilters.vegan && !meal.isVegan) {
+        //   return false;
+        // }
+        // if (appliedFilters.vegetarian && !meal.isVegetarian) {
+        //   return false;
+        // }
+        // if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+        //   return false;
+        // }
+        // return true;
+        return (
+          (meal.isGlutenFree || !appliedFilters.glutenFree) &&
+          (meal.isVegan || !appliedFilters.vegan) &&
+          (meal.isVegetarian || !appliedFilters.vegetarian) &&
+          (meal.isLactoseFree || !appliedFilters.lactoseFree)
+        );
+      });
+      return { ...state, filteredMeals: newFilteredMeals };
+    }
+    default:
+      return state;
+  }
 };
 
 export default mealsReducer;

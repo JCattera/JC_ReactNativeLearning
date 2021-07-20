ae7c0f0441fb527582dc416b66e87386d00332d6
JCattera <jfcatterall@gmail.com>
2021-07-20 09:24:21 -0400

    cart actions/reducer

--- Section8_ShopApp/store/actions/cart.js
+++ Section8_ShopApp/store/actions/cart.js
@@ -1,5 +1,6 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const CLEAR_CART_ITEM = 'CLEAR_CART_ITEM';
 
 export const addToCart = (product) => {
   return { type: ADD_TO_CART, addedProduct: product };
@@ -7,4 +8,8 @@ export const addToCart = (product) => {
 
 export const removeFromCart = (id) => {
   return { type: REMOVE_FROM_CART, itemId: id };
+};
+
+export const clearCartItem = (id) => {
+  return { type: CLEAR_CART_ITEM, itemId: id };
 };
--- Section8_ShopApp/store/reducers/cart.js
+++ Section8_ShopApp/store/reducers/cart.js
@@ -1,27 +1,54 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART_ITEM,
+} from '../actions/cart';
 import CartItem from '../../models/cart-item';
 const initialState = {
   cartItems: {},
-  totalItems: 0,
+  totalCartAmount: 0,
 };
 
-const cartReducer = (state = initialState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
-    case ADD_TO_CART: {
+    case ADD_TO_CART:
       const addedProduct = action.addedProduct;
       const productPrice = addedProduct.price;
       const productTitle = addedProduct.title;
+      let newItem;
       if (state.cartItems[addedProduct.id]) {
-        const currentItem = state.cartItems[addedProduct.id];
-        const newItem = CartItem(
-          currentItem.quantity + 1,
+        newItem = new CartItem(
+          state.cartItems[addedProduct.id].quantity + 1,
           productPrice,
           productTitle,
-          currentItem.sumTotal + productPrice
+          state.cartItems[addedProduct.id].sumTotal + productPrice
         );
       } else {
+        newItem = new CartItem(1, productPrice, productTitle, productPrice);
       }
-    }
+      return {
+        ...state,
+        cartItems: { ...state.cartItems, [addedProduct.id]: newItem },
+        totalCartAmount: state.totalCartAmount + productPrice,
+      };
+
+    case CLEAR_CART_ITEM:
+      const currentCartItems = { ...state.cartItems };
+      if (state.cartItems[action.id]) {
+        priceSubtracted = state.cartItems[action.id].sumTotal;
+        delete currentCartItems[action.id];
+        return {
+          ...state,
+          cartItems: currentCartItems,
+          totalCartAmount: state.totalCartAmount - priceSubtracted,
+        };
+      } else {
+        return state;
+      }
+    // case REMOVE_FROM_CART:
+    //   const currentCartItems = { ...state.cartItems };
+    //   const priceSubtracted = state.cartItems[action.id].price;
+    //   return state;
   }
   return state;
 };

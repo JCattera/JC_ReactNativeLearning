19084407f469477957840175425586f1863e7ebe
JCattera <jfcatterall@gmail.com>
2021-07-20 09:26:11 -0400

    moving header button to separate folder

--- Section8_ShopApp/screens/shop/ProductsOverviewScreen.js
+++ Section8_ShopApp/screens/shop/ProductsOverviewScreen.js
@@ -3,13 +3,11 @@ import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import ProductList from '../../components/ProductList';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import HeaderButton from '../../components/HeaderButton';
-
+import HeaderButton from '../../components/UI/HeaderButton';
 const ProductsOverviewScreen = (props) => {
   const availableProducts = useSelector(
     (state) => state.products.availableProducts
   );
-
   return (
     <ProductList
       displayedProducts={availableProducts}
